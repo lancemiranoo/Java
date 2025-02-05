a14a54142c70e3084e38550c64222a47e73abbe6
lancemiranoo <lancemirano01@gmail.com>
2025-02-06 04:38:56 +0800

    Updated files

---
+++ Full Stack Java/Week 5/databasesecurity/src/main/java/spring/security/database/databasesecurity/DatabasesecurityApplication.java
@@ -0,0 +1,13 @@
+package spring.security.database.databasesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasesecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasesecurityApplication.class, args);
+	}
+
+}
---
+++ Full Stack Java/Week 5/databasesecurity/src/test/java/spring/security/database/databasesecurity/DatabasesecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.database.databasesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasesecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
